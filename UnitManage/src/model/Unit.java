257631e14293cbda6d838443a72ea74c015ef4cb
roshani <dissanayakeroshi@gmail.com>
2022-04-24 20:17:34 +0530

    intial Commit

---
+++ UnitManage/src/model/Unit.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Unit {
+
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/electricity?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+					"root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+}
