ad7ca88d34caadec945c9e55daf4406de8f9f21d
roshani <dissanayakeroshi@gmail.com>
2022-04-24 20:26:44 +0530

    unit.java

--- UnitManage/src/model/Unit.java
+++ UnitManage/src/model/Unit.java
@@ -52,6 +52,26 @@ public String insertUnit(String user_account_no, String usage_date, String used_
 		}
 		return output;
 	}
+public String readUnit() {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			// Prepare the html table to be displayed
+			output = "<table border=\"1\"><tr><th>Unit ID</th><th>Account No</th><th>Date</th><th>Unit Amount</th><th>Price for PerUnit</th><th>total_price Amount</th></tr>";
+			String query = "select * from electricity_power_unit";
+			Statement stmt = (Statement) con.createStatement();
+			ResultSet rs = ((java.sql.Statement) stmt).executeQuery(query);
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				String unit_id = Integer.toString(rs.getInt("unit_id"));
+				String user_account_no = rs.getString("user_account_no");
+				String usage_date = rs.getString("usage_date");
+				String used_units = rs.getString("used_units");
+				String price_per_unit = rs.getString("price_per_unit");
+				String total_price = rs.getString("total_price");
 
 
 }
