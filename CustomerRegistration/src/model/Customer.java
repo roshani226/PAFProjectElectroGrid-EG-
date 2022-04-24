0731b67bd562d827690674735003c5b87b2f282a
Pasindu <Pasindu@DESKTOP-QN26FVR>
2022-04-24 17:55:02 +0530

    second commit

--- CustomerRegistration/src/model/Customer.java
+++ CustomerRegistration/src/model/Customer.java
@@ -23,5 +23,34 @@ public class Customer {
 		}
 		return con;
 	}
+	
+	public String insertCustomer(String cName, String cAddress, String cEmail, String cDate, String pno) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			// create a prepared statement
+			String query = " insert into customer1(`cID`,`cName`,`cAddress`,`cEmail`,`cDate`,`pno`)"
+					+ " values (?, ?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, cName);
+			preparedStmt.setString(3, cAddress);
+			preparedStmt.setString(4, cEmail);
+			preparedStmt.setString(5, cDate);
+			preparedStmt.setString(6, pno);
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Inserted successfully";
+		} catch (Exception e) {
+			output = "Error while inserting the customer.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
 
 	 
