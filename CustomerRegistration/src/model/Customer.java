7ee0bb0f39e2e20bd483978507e1449fa0efa022
Pasindu <Pasindu@DESKTOP-QN26FVR>
2022-04-24 14:07:22 +0530

    first commit

---
+++ CustomerRegistration/src/model/Customer.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Customer {
+
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/power?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+					"root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	 
