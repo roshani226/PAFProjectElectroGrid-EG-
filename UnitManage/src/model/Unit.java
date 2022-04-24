27eb790bf81a86155c598c0abbea1b9cf2573ceb
roshani <dissanayakeroshi@gmail.com>
2022-04-24 20:52:27 +0530

    unit.java5

--- UnitManage/src/model/Unit.java
+++ UnitManage/src/model/Unit.java
@@ -146,6 +146,9 @@ public String deleteUnit(String unit_id) {
 			con.close();
 			preparedStmt.setInt(6, Integer.parseInt(unit_id));	}
 }
+			output = "Deleted successfully";
+		} catch (Exception e) {
+			output = "Error while deleting the unit.";
 
 
 
