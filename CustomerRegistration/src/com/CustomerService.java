a00a4e5a40413076f85147946e1ce7bdd78d29dc
Pasindu <Pasindu@DESKTOP-QN26FVR>
2022-04-25 09:59:40 +0530

    CustomerService1

---
+++ CustomerRegistration/src/com/CustomerService.java
@@ -0,0 +1,18 @@
+package com;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML 
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+import model.Customer;
+
+@Path("/Customer")
+public class CustomerService {
+	Customer registerObj = new Customer();
+
+	 
