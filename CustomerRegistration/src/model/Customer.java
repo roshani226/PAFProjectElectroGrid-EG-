f541a78d13a565dbb5275cb4f131cf5e710d41fb
Pasindu <Pasindu@DESKTOP-QN26FVR>
2022-04-25 09:26:14 +0530

    fifth commit

--- CustomerRegistration/src/model/Customer.java
+++ CustomerRegistration/src/model/Customer.java
@@ -93,5 +93,45 @@ public class Customer {
 		}
 		return output;
 	}
+	
+	
+	 
+
+	 
+		public String updateCustomer(String cID, String cName, String cAddress, String cEmail, String cDate, String pno) {
+			String output = "";
+
+			try {
+				Connection con = connect();
+
+				if (con == null) {
+					return "Error while connecting to the database for updating.";
+				}
+
+				// create a prepared statement
+				String query = "UPDATE customer1 SET cName=?,cAddress=?,cEmail=?,cDate=?,pno=?" + "WHERE cID=?";
+
+				PreparedStatement preparedStmt = con.prepareStatement(query);
+
+				// binding values
+				preparedStmt.setString(1, cName);
+				preparedStmt.setString(2, cAddress);
+				preparedStmt.setString(3, cEmail);
+				preparedStmt.setString(4, cDate);
+				preparedStmt.setString(5, pno);
+				preparedStmt.setInt(6, Integer.parseInt(cID));
+
+				// execute the statement
+				preparedStmt.execute();
+				con.close();
+
+				output = "Updated successfully";
+			} catch (Exception e) {
+				output = "Error while updating the customer.";
+				System.err.println(e.getMessage());
+			}
+
+			return output;
+		}
 
 	 
