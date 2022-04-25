88afa7b438a2b635a00e0f72d3bf77cb397ca6a9
Pasindu <Pasindu@DESKTOP-QN26FVR>
2022-04-25 21:29:50 +0530

    delete

--- CustomerRegistration/src/com/CustomerService.java
+++ CustomerRegistration/src/com/CustomerService.java
@@ -56,3 +56,21 @@ public class CustomerService {
 	return output;
 	} 
 	
+	
+
+	 
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteCustomer(String customerData)
+	{
+	//Convert the input string to an XML document
+	 Document doc = Jsoup.parse(customerData, "", Parser.xmlParser());
+
+	//Read the value from the element <ID>
+	 String cID = doc.select("cID").text();
+	 String output = registerObj.deleteCustomer(cID);
+	return output;
+	}
+}
